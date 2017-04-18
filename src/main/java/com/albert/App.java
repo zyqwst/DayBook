4078cbea35e3f36fb26c70c00006c1e120204ad8
AlbertZhang <790321193@qq.com>
2017-04-18 21:35:51 +0800

    ---

--- src/main/java/com/albert/App.java
+++ src/main/java/com/albert/App.java
@@ -2,6 +2,7 @@ package com.albert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 
 /**
  * Hello world!
