7da21a583096d9c7441b9b51d03a7c8ad357427c
albert <790321193@qq.com>
2017-05-03 17:00:49 +0800

    ---

--- src/main/java/com/albert/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/albert/interceptor/AuthorizationInterceptor.java
@@ -7,7 +7,6 @@ import java.lang.reflect.Method;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
